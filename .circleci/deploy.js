74f7a80ad2d07a063abb4ed15133530a78a5627f
jordan.blum <jordan.blum@workday.com>
2019-04-26 10:52:55 -0700

    more troubleshooting

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -7,7 +7,7 @@ var config = {
     password: process.env.FTPPASS,
     host: process.env.FTPHOST,
     port: 21,
-    localRoot: __dirname + "/",
+    localRoot: __dirname + "~/",
     remoteRoot: "/",
     include: ['*']
 }
